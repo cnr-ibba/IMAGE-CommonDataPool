fb2af15d2ffdd3eb7473b6efed1eedfcb55cfa9d
Alexey Sokolov <alexey@ebi.ac.uk>
2018-10-29 15:27:49 +0000

    Adding logic for detail views

--- image_backend/backend/urls.py
+++ image_backend/backend/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('organism/', views.ListOrganismsView.as_view(), name='organismindex'),
-    path('specimen/', views.ListSpecimensView.as_view(), name='specimenindex')
+    path('organism/<organism_id>/', views.OrganismsDetailsView.as_view(), name='organismdetail'),
+    path('specimen/', views.ListSpecimensView.as_view(), name='specimenindex'),
+    path('specimen/<specimen_id>/', views.SpecimensDetailsView.as_view(), name='specimendetail'),
 ]
--- image_backend/backend/views.py
+++ image_backend/backend/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import status
 
 from .models import SampleInfo
 from .serializers import SpecimensSerializer, OrganismsSerializer
@@ -11,8 +13,42 @@ class ListSpecimensView(generics.ListAPIView):
         return SampleInfo.objects.filter(specimens__isnull=False)
 
 
+class SpecimensDetailsView(generics.RetrieveAPIView):
+    queryset = SampleInfo.objects.all()
+    serializer_class = SpecimensSerializer
+
+    def get(self, request, *a, **kw):
+        try:
+            organism = self.queryset.get(data_source_id=kw['specimen_id'])
+            return Response(SpecimensSerializer(organism).data)
+        except:
+            return Response(
+                data={
+                    "message": "Organism with id: {} does not exist".format(kw['specimen_id'])
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+
 class ListOrganismsView(generics.ListAPIView):
     serializer_class = OrganismsSerializer
 
     def get_queryset(self):
         return SampleInfo.objects.filter(organisms__isnull=False)
+
+
+class OrganismsDetailsView(generics.RetrieveAPIView):
+    queryset = SampleInfo.objects.all()
+    serializer_class = OrganismsSerializer
+
+    def get(self, request, *a, **kw):
+        try:
+            organism = self.queryset.get(data_source_id=kw['organism_id'])
+            return Response(OrganismsSerializer(organism).data)
+        except:
+            return Response(
+                data={
+                    "message": "Organism with id: {} does not exist".format(kw['organism_id'])
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
