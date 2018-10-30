db6e20d78e68d80971174e5e111e58547ca2a6c5
Alexey Sokolov <alexey@ebi.ac.uk>
2018-10-30 10:57:47 +0000

    Allow write data into nested serializers

--- image_backend/backend/serializers.py
+++ image_backend/backend/serializers.py
@@ -6,17 +6,21 @@ from .models import SampleInfo, AnimalInfo, SampleDataInfo
 class SampleDataInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = SampleDataInfo
-        fields = "__all__"
+        fields = ('derived_from', 'collection_date', 'collection_place', 'collection_place_accuracy', 'organism_part',
+                  'specimen_collection_protocol', 'collection_place_latitude', 'collection_place_longitude',
+                  'developmental_stage', 'physiological_stage', 'availability', 'sample_storage',
+                  'sample_storage_processing', 'animal_age_at_collection', 'sampling_to_preparation_interval')
 
 
 class AnimalInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = AnimalInfo
-        fields = "__all__"
+        fields = ('supplied_breed', 'efabis_breed_country', 'sex', 'birth_location_accuracy', 'mapped_breed',
+                  'birth_location', 'birth_location_longitude', 'birth_location_latitude', 'child_of')
 
 
 class SpecimensSerializer(serializers.ModelSerializer):
-    specimens = SampleDataInfoSerializer(many=True, read_only=True)
+    specimens = SampleDataInfoSerializer(many=True)
 
     class Meta:
         model = SampleInfo
@@ -26,9 +30,16 @@ class SpecimensSerializer(serializers.ModelSerializer):
                   'submission_description', 'person_first_name', 'organization_address', 'organization_country',
                   'description', 'person_initial', 'organization_uri', 'publication_doi', 'specimens')
 
+    def create(self, validated_data):
+        specimens_data = validated_data.pop('specimens')
+        sample = SampleInfo.objects.create(**validated_data)
+        for specimen in specimens_data:
+            SampleDataInfo.objects.create(sample=sample, **specimen)
+        return sample
+
 
 class OrganismsSerializer(serializers.ModelSerializer):
-    organisms = AnimalInfoSerializer(many=True, read_only=True)
+    organisms = AnimalInfoSerializer(many=True)
 
     class Meta:
         model = SampleInfo
@@ -37,3 +48,10 @@ class OrganismsSerializer(serializers.ModelSerializer):
                   'gene_bank_name', 'gene_bank_country', 'data_source_type', 'data_source_version', 'species',
                   'submission_description', 'person_first_name', 'organization_address', 'organization_country',
                   'description', 'person_initial', 'organization_uri', 'publication_doi', 'organisms')
+
+    def create(self, validated_data):
+        organisms_data = validated_data.pop('organisms')
+        sample = SampleInfo.objects.create(**validated_data)
+        for organism in organisms_data:
+            AnimalInfo.objects.create(sample=sample, **organism)
+        return sample
