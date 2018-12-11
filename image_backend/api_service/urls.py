a486b1968938d9eb0e7623059a6494d61644f8ab
Alexey Sokolov <alexey@ebi.ac.uk>
2018-12-11 11:32:36 +0000

    Removing frontend from docker-compose

--- image_backend/api_service/urls.py
+++ image_backend/api_service/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', admin.site.urls),
-    path('api/admin/', admin.site.urls),
-    path('api/backend/', include('backend.urls')),
-    path('api/api-auth/', include('rest_framework.urls')),
+    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('backend/', include('backend.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
