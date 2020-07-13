eefae160c1e30ee6c419ab2a0c4a3806b5e6883c
Paolo Cozzi <bunop@libero.it>
2020-07-13 19:30:09 +0200

    :bug: fix docker configuration and create postgis template

--- postgis/docker-entrypoint-initdb.d/02-create_database.sh
+++ postgis/docker-entrypoint-initdb.d/02-create_database.sh
@@ -3,5 +3,5 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   -- Create a GIS database
-  CREATE DATABASE $DATABASE_NAME WITH owner = postgres ENCODING = 'UTF8' CONNECTION LIMIT = -1;
+  CREATE DATABASE $DATABASE_NAME TEMPLATE template_postgis;
 EOSQL
--- postgis/docker-entrypoint-initdb.d/02-install_extension.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" cdp <<-EOSQL
-  -- Install GIS extension
-  CREATE EXTENSION postgis;
-  CREATE EXTENSION postgis_raster;
-EOSQL
