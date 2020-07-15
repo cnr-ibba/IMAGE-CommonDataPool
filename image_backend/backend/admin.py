c7a0a3116f12adcc732902b080b665f61cace5af
Paolo Cozzi <bunop@libero.it>
2020-07-15 18:16:23 +0200

    :wrench: customize admin

--- image_backend/backend/admin.py
+++ image_backend/backend/admin.py
@@ -1,6 +1,24 @@
-from django.contrib import admin
+
+from django.contrib.gis import admin
+
 from .models import (
     Organism, Specimen, Species2CommonName, DADISLink)
+
+
+class OrganismAdmin(admin.OSMGeoAdmin):
+    list_display = (
+        'data_source_id', 'gene_bank_country', 'species', 'person_email',
+        'supplied_breed')
+    list_filter = ['person_email', 'species', 'gene_bank_country']
+    search_fields = ['data_source_id', 'supplied_breed']
+
+
+class SpecimenAdmin(admin.OSMGeoAdmin):
+    list_display = (
+        'data_source_id', 'gene_bank_country', 'species', 'person_email',
+        'organism_part', 'derived_from')
+    list_filter = ['person_email', 'species', 'gene_bank_country']
+    search_fields = ['data_source_id', 'derived_from']
 
 
 class Species2CommonNameAdmin(admin.ModelAdmin):
@@ -13,7 +31,7 @@ class DADISLinkAdmin(admin.ModelAdmin):
     )
 
 
-admin.site.register(Organism)
-admin.site.register(Specimen)
+admin.site.register(Organism, OrganismAdmin)
+admin.site.register(Specimen, SpecimenAdmin)
 admin.site.register(Species2CommonName, Species2CommonNameAdmin)
 admin.site.register(DADISLink, DADISLinkAdmin)
